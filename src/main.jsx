2dd1e5bc7716a19c341b81493e606ee855c84b85
ivantomicic-crowdhouse <ivan@tomicic.dev>
2025-03-02 12:50:17 +0100

    Installed tailwind 3 and heroui

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HeroUIProvider } from "@heroui/react";
 import App from "./App.jsx";
+import "./main.css";
 import "./styles/index.scss";
 
 const queryClient = new QueryClient({
@@ -16,8 +18,10 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-		</QueryClientProvider>
+		<HeroUIProvider>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
+		</HeroUIProvider>
 	</StrictMode>
 );
